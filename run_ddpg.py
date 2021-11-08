6f684f06ec3b0754892ef6625677617c837ac708
goncamateus <mgm4@cin.ufpe.br>
2021-11-08 09:53:30 -0300

    :bug: Fixes a minor bug

--- run_ddpg.py
+++ run_ddpg.py
@@ -20,17 +20,17 @@ def main():
         DEVICE=torch.device("cuda"),
         ENV_NAME="VSS-v0",
         LEARNING_RATE=1e-3,
-        BATCH_SIZE=256,
+        BATCH_SIZE=64,
         GAMMA=0.95,
         NOISE_SIGMA_INITIAL=0.8,
-        NOISE_THETA=0.15,
+        NOISE_THETA=0.1,
         NOISE_SIGMA_DECAY=0.99,
-        NOISE_SIGMA_MIN=0.15,
-        NOISE_SIGMA_GRAD_STEPS=3000,
-        REPLAY_SIZE=500000,
+        NOISE_SIGMA_MIN=0.1,
+        NOISE_SIGMA_GRAD_STEPS=300,
+        REPLAY_SIZE=5000000,
         REPLAY_INITIAL=100000,
         SAVE_FREQUENCY=100000,
-        TOTAL_GRAD_STEPS=1000000,
+        TOTAL_GRAD_STEPS=10000000,
     )
     env = gym.wrappers.Monitor(
         gym.make(hp.ENV_NAME), "./monitor/", force=True, video_callable=schedule
@@ -54,9 +54,10 @@ def main():
 
     noise.reset()
     state = env.reset()
-    steps = 0
+    grad_steps = 0
 
     epi_reward = 0
+    epi_steps = 0
     for i in tqdm(range(1, hp.TOTAL_GRAD_STEPS + hp.REPLAY_INITIAL), smoothing=0.1):
         log_dict = {}
         action = agent.act(state)
@@ -65,6 +66,7 @@ def main():
         agent.replay_buffer.add(state, action, reward, next_state, done)
         state = next_state
         epi_reward += reward
+        epi_steps += 1
 
         if done:
             info = {
@@ -74,30 +76,32 @@ def main():
             }
             log_dict.update(info)
             log_dict["ep_info/ep_rw"] = epi_reward
+            log_dict["ep_info/ep_steps"] = epi_steps
             noise.reset()
             state = env.reset()
             epi_reward = 0
+            epi_steps = 0
 
         if len(agent.replay_buffer) >= hp.REPLAY_INITIAL and i % 10 == 0:
             train_logs = agent.train()
             log_dict.update(train_logs)
-            steps += 1
+            grad_steps += 1
+            if (
+                hp.NOISE_SIGMA_DECAY
+                and noise.sigma > hp.NOISE_SIGMA_MIN
+                and grad_steps % hp.NOISE_SIGMA_GRAD_STEPS == 0
+            ):
+                noise.sigma *= hp.NOISE_SIGMA_DECAY
+                log_dict["train/noise"] = noise.sigma
             wandb.log(log_dict)
 
-        if (
-            hp.NOISE_SIGMA_DECAY
-            and noise.sigma > hp.NOISE_SIGMA_MIN
-            and steps % hp.NOISE_SIGMA_GRAD_STEPS == 0
-        ):
-            noise.sigma *= hp.NOISE_SIGMA_DECAY
-
-        if steps % hp.SAVE_FREQUENCY == 0:
+        if grad_steps % hp.SAVE_FREQUENCY == 0 and grad_steps > 0:
             nets = {"actor": agent.actor, "critic": agent.critic}
             optims = {
                 "actor_optimizer": agent.actor_optimizer,
                 "critic_optimizer": agent.critic_optimizer,
             }
-            save_checkpoint(hp, steps, nets, optims)
+            save_checkpoint(hp, grad_steps, nets, optims)
 
 
 if __name__ == "__main__":
