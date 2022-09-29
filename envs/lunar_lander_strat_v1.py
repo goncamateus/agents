1441872e4c4ad96539495ff6e6644a0af0de67fd
goncamateus <mgm4@cin.ufpe.br>
2022-09-29 13:13:32 -0300

    closest to msc release

--- envs/lunar_lander_strat_v1.py
+++ envs/lunar_lander_strat_v1.py
@@ -2,7 +2,7 @@ import numpy as np
 from gym.envs.box2d.lunar_lander import LunarLander
 
 
-class LunarLanderStratV1(LunarLander):
+class LunarLanderStrat(LunarLander):
     def __init__(self, stratified=True):
         super().__init__()
         self.cumulative_reward_info = {
@@ -19,11 +19,7 @@ class LunarLanderStratV1(LunarLander):
             "Original_reward": 0,
         }
         self.stratified = stratified
-        self.prev_rew = None
-        self.strat_prev_rew = None
-        self.ori_weights = np.array(
-            [100.0, 100.0, 100.0, 100.0, 100.0, 0.3, 0.03, 10.0, 10.0, 100.0]
-        )
+        self.prev_rew = np.zeros(10)
         self.num_rewards = 10
 
     def step(self, action):
@@ -53,58 +49,56 @@ class LunarLanderStratV1(LunarLander):
                 s_power = 1.0
 
         state, reward, done, info = super().step(action)
-        shaping = np.zeros(self.num_rewards)
-        strat_reward = np.zeros(self.num_rewards)
+        strat_reward = np.zeros(10)
         # Distance to center
-        shaping[0] = -abs(state[0])
-        shaping[1] = -abs(state[1])
+        strat_reward[0] = -abs(state[0]) - self.prev_rew[0]
+        self.prev_rew[0] = -abs(state[0])
+        strat_reward[1] = -abs(state[1]) - self.prev_rew[1]
+        self.prev_rew[1] = -abs(state[1])
+        self.cumulative_reward_info["reward_Distance_X"] += strat_reward[0]
+        self.cumulative_reward_info["reward_Distance_Y"] += strat_reward[1]
         # Speed discount
-        shaping[2] = -abs(state[2])
-        shaping[3] = -abs(state[3])
+        strat_reward[2] = -abs(state[2]) - self.prev_rew[2]
+        self.prev_rew[2] = -abs(state[2])
+        strat_reward[3] = -abs(state[3]) - self.prev_rew[3]
+        self.prev_rew[3] = -abs(state[3])
+        self.cumulative_reward_info["reward_Speed_X"] += strat_reward[2]
+        self.cumulative_reward_info["reward_Speed_Y"] += strat_reward[3]
         # Angle discount
-        shaping[4] = -abs(state[4])
-        # Ground Contacts
-        shaping[7] = state[6]
-        shaping[8] = state[7]
-        if self.prev_rew is not None:
-            strat_reward = shaping - self.prev_rew
+        strat_reward[4] = -abs(state[4]) - self.prev_rew[4]
+        self.prev_rew[4] = -abs(state[4])
+        self.cumulative_reward_info["reward_Angle"] += strat_reward[4]
         # Power discount
-        strat_reward[5] = -m_power
-        strat_reward[6] = -s_power
+        strat_reward[5] = -m_power - self.prev_rew[5]
+        self.prev_rew[5] = -m_power
+        strat_reward[6] = -s_power - self.prev_rew[6]
+        self.prev_rew[6] = -s_power
+        self.cumulative_reward_info["reward_Power_linear"] += strat_reward[5]
+        self.cumulative_reward_info["reward_Power_angular"] += strat_reward[6]
+        # Ground Contacts
+        strat_reward[7] = state[6] - self.prev_rew[7]
+        self.prev_rew[7] = state[6]
+        strat_reward[8] = state[7] - self.prev_rew[8]
+        self.prev_rew[8] = state[7]
+        self.cumulative_reward_info["reward_Left_contact"] += strat_reward[7]
+        self.cumulative_reward_info["reward_Right_contact"] += strat_reward[8]
 
         # Win/Lost
         if done:
-            self.prev_rew = None
-            shaping = np.zeros(self.num_rewards)
-            strat_reward = np.zeros(self.num_rewards)
-            if self.game_over or abs(state[0]) >= 1.0:
-                strat_reward[9] = -1
             if not self.lander.awake:
                 strat_reward[9] = 1
+            else:
+                strat_reward[9] = -1
 
         if reward == 0:
             strat_reward = np.zeros(self.num_rewards)
 
-        self.prev_rew = shaping
-        self.prev_rew[5] = 0
-        self.prev_rew[6] = 0
-
-        self.cumulative_reward_info["reward_Distance_X"] += strat_reward[0]
-        self.cumulative_reward_info["reward_Distance_Y"] += strat_reward[1]
-        self.cumulative_reward_info["reward_Speed_X"] += strat_reward[2]
-        self.cumulative_reward_info["reward_Speed_Y"] += strat_reward[3]
-        self.cumulative_reward_info["reward_Angle"] += strat_reward[4]
-        self.cumulative_reward_info["reward_Power_linear"] += strat_reward[5]
-        self.cumulative_reward_info["reward_Power_angular"] += strat_reward[6]
-        self.cumulative_reward_info["reward_Left_contact"] += strat_reward[7]
-        self.cumulative_reward_info["reward_Right_contact"] += strat_reward[8]
         self.cumulative_reward_info["reward_Goal"] += strat_reward[9]
 
         self.cumulative_reward_info["Original_reward"] += reward
         info.update(self.cumulative_reward_info)
-        reward = strat_reward
         if done:
-            self.prev_rew = None
+            self.prev_rew = np.zeros(10)
             self.cumulative_reward_info = {
                 "reward_Distance_X": 0,
                 "reward_Distance_Y": 0,
@@ -118,8 +112,8 @@ class LunarLanderStratV1(LunarLander):
                 "reward_Goal": 0,
                 "Original_reward": 0,
             }
-        return state, reward, done, info
+        return state, strat_reward, done, info
 
 
-class LunarLanderContinuousStratV1(LunarLanderStratV1):
+class LunarLanderContinuousStrat(LunarLanderStrat):
     continuous = True
