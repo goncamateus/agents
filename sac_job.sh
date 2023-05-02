6958cac078419bd33abf14f50509676e254bd60c
goncamateus <mgm4@cin.ufpe.br>
2023-05-02 20:12:48 -0300

    adds bash scripts for slurm

---
+++ sac_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=sac_agent
+#SBATCH --ntasks=1
+#SBATCH --mem 64G
+#SBATCH -c 32
+#SBATCH -o sac_agent.log
+#SBATCH --gpus=1
+
+# Load modules and activate python environment
+module load Python3.10 Xvfb freeglut
+source $HOME/.pyenvs/rl/bin/activate
+
+# Run the script
+python run_sac.py -cuda --gym-id $1 --total-timesteps 1000000 --capture-video --num-envs 16 --track
