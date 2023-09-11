e41f4bfb04ab9feb7dd360ff70698b087be1e5d3
goncamateus <mgm4@cin.ufpe.br>
2023-09-11 09:35:58 -0300

    WIP DQN+drDQN+DyLam-DQN

---
+++ methods/dqn.py
@@ -0,0 +1,177 @@
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from utils.buffer import ReplayBuffer
+from utils.experiment import StratLastRewards
+
+
+class DQNAgent(nn.Module):
+    def __init__(
+        self,
+        hyper_params,
+        observation_space,
+        action_space,
+    ):
+        super(DQNAgent, self).__init__()
+        self.observation_space = observation_space
+        self.action_space = action_space
+        self.alpha = hyper_params.learning_rate
+        self.gamma = hyper_params.gamma
+        self.num_rewards = hyper_params.num_rewards
+        self.epsilon = hyper_params.epsilon
+        self.epsilon_end = hyper_params.epsilon_end
+        self.epsilon_decay = hyper_params.epsilon_decay
+        self.target_update_freq = hyper_params.target_update_freq
+        self.n_epochs = hyper_params.num_epochs
+        self.n_updates = 0
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() and hyper_params.cuda else "cpu"
+        )
+        self.replay_buffer = ReplayBuffer(hyper_params.buffer_size, self.device)
+        self.set_net()
+
+        self.rew_tau = hyper_params.rew_tau
+        self.rew_max = np.array(hyper_params.r_max, dtype=np.float32)
+        self.rew_min = np.array(hyper_params.r_min, dtype=np.float32)
+        self.drQ = not hyper_params.dylam
+        self.stratified = hyper_params.stratified
+        self.rb_rewards = StratLastRewards(hyper_params.episodes_rb, self.num_rewards)
+        self.last_rew_mean = None
+
+    def set_net(self):
+        obs_size = (
+            self.observation_space.n
+            if isinstance(self.observation_space, gym.spaces.Discrete)
+            else self.observation_space.shape[0]
+        )
+
+        def my_model():
+            return nn.Sequential(
+                nn.Linear(obs_size, 256),
+                nn.ReLU(),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                nn.Linear(256, self.action_space.n),
+            ).to(self.device)
+
+        self.dqn = my_model()
+        self.target_dqn = my_model()
+        self.target_dqn.load_state_dict(self.dqn.state_dict())
+        self.optimizer = torch.optim.Adam(self.dqn.parameters(), lr=self.alpha)
+        self.comp_dqns = nn.ModuleList([my_model()] * self.num_rewards).to(self.device)
+        self.comp_target_dqns = nn.ModuleList([my_model()] * self.num_rewards).to(
+            self.device
+        )
+        self.comp_optimizers = []
+        for i in range(self.num_rewards):
+            self.comp_target_dqns[i].load_state_dict(self.comp_dqns[i].state_dict())
+            self.comp_optimizers.append(
+                torch.optim.Adam(self.comp_dqns[i].parameters(), lr=self.alpha)
+            )
+
+    def get_action(self, state):
+        if np.random.random() > self.epsilon:
+            q_values = self.dqn(torch.FloatTensor(state).to(self.device))
+            action = q_values.argmax().item()
+        else:
+            action = self.action_space.sample()
+            self.epsilon = max(self.epsilon_end, self.epsilon * self.epsilon_decay)
+        return action
+
+    def store_reward(self, rewards):
+        self.rb_rewards.add(rewards)
+
+    def dylam(self):
+        if self.rb_rewards.can_do() and not self.drQ:
+            rew_mean_t = self.rb_rewards.mean()
+            if self.last_rew_mean is not None:
+                rew_mean_t = (
+                    rew_mean_t + (self.last_rew_mean - rew_mean_t) * self.rew_tau
+                )
+            dQ = np.clip(
+                (self.rew_max - rew_mean_t) / (self.rew_max - self.rew_min), 0, 1
+            )
+            expdQ = np.exp(dQ) - 1
+            self.lambdas = expdQ / (np.sum(expdQ, 0) + 1e-4)
+            self.last_rew_mean = rew_mean_t
+
+    def update_components(self, batch):
+        (
+            state_batch,
+            action_batch,
+            reward_batch,
+            next_state_batch,
+            done_batch,
+        ) = batch
+        if self.num_rewards == 1:
+            reward_batch = reward_batch.unsqueeze(1)
+        self.n_updates += 1
+        losses = []
+        for i in range(len(self.num_rewards)):
+            with torch.no_grad():
+                next_q_values = (
+                    self.comp_target_dqns[i](next_state_batch)
+                    .max(1)
+                    .values.unsqueeze(1)
+                )
+                next_q_values[done_batch] = 0.0
+            y = reward_batch[:, i] + self.gamma * next_q_values
+            q_values = self.comp_dqns[i](state_batch)
+            q_values = q_values.gather(1, action_batch)
+            loss = F.mse_loss(q_values, y)
+            self.comp_optimizers[i].zero_grad()
+            loss.backward()
+            losses.append(loss.item())
+            self.comp_optimizers[i].step()
+            if self.n_updates % self.target_update_freq == 0:
+                self.comp_target_dqns[i].load_state_dict(self.comp_dqns[i].state_dict())
+        return losses
+
+    def update(self, batch_size, lambdas):
+        comp_losses = []
+        losses = []
+        for _ in range(self.n_epochs):
+            batch = self.replay_buffer.sample(batch_size)
+            (
+                state_batch,
+                action_batch,
+                reward_batch,
+                next_state_batch,
+                done_batch,
+            ) = batch
+            if self.stratified:
+                comp_loss = self.update_components(batch)
+                comp_losses.append(comp_loss)
+                Qs = torch.zeros(self.num_rewards).to(self.device)
+                with torch.no_grad():
+                    for i in range(self.num_rewards):
+                        Qs[i] = self.comp_dqns[i](state_batch).gather(1, action_batch)
+                    Qs = torch.sum(lambdas * Qs)
+                actual_Qs = self.dqn(state_batch).gather(1, action_batch)
+                loss = F.mse_loss(actual_Qs, Qs)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                losses.append(loss.item())
+            else:
+                with torch.no_grad():
+                    next_q_values = (
+                        self.target_dqn(next_state_batch).max(1).values.unsqueeze(1)
+                    )
+                    next_q_values[done_batch] = 0.0
+                y = reward_batch[:, i] + self.gamma * next_q_values
+                q_values = self.dqn(state_batch)
+                q_values = q_values.gather(1, action_batch)
+                loss = F.mse_loss(q_values, y)
+                self.optimizer.zero_grad()
+                loss.backward()
+                losses.append(loss.item())
+                self.optimizer.step()
+                if self.n_updates % self.target_update_freq == 0:
+                    self.target_dqn.load_state_dict(self.dqn.state_dict())
+        mean_losses = np.mean(losses)
+        mean_comp_losses = np.mean(comp_losses, axis=0)
+        return mean_losses, mean_comp_losses
---
+++ run_dqn.py
@@ -0,0 +1,234 @@
+# https://github.com/vwxyzjn/cleanrl/blob/master/cleanrl/ppo.py
+import argparse
+import json
+import os
+import random
+import time
+from distutils.util import strtobool
+
+import gymnasium as gym
+import mo_gymnasium as mo_gym
+import numpy as np
+import torch
+from pyvirtualdisplay import Display
+from torch.utils.tensorboard import SummaryWriter
+
+import envs
+import wandb
+
+from methods.dqn import DQNAgent
+
+
+def parse_args():
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
+        help="the name of this experiment")
+    parser.add_argument("--gym-id", type=str, default="LunarLanderOri-v0",
+        help="the id of the gym environment")
+    parser.add_argument("--learning-rate", type=float, default=2.5e-4,
+        help="the learning rate of the optimizer")
+    parser.add_argument("--seed", type=int, default=0,
+        help="seed of the experiment")
+    parser.add_argument("--total-timesteps", type=int, default=1000000,
+        help="total timesteps of the experiments")
+    parser.add_argument("--torch-deterministic", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="if toggled, `torch.backends.cudnn.deterministic=False`")
+    parser.add_argument("--cuda", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="if toggled, cuda will be enabled by default")
+    parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="weather to capture videos of the agent performances (check out `videos` folder)")
+    parser.add_argument("--video-freq", type=int, default=50,
+        help="Frequency of saving videos, in episodes")    
+    parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="Log on wandb")
+
+    # Algorithm specific arguments
+    parser.add_argument("--num-envs", type=int, default=1,
+        help="the number of parallel game environments")
+    parser.add_argument("--buffer-size", type=int, default=int(1e6),
+        help="the replay memory buffer size")
+    parser.add_argument("--gamma", type=float, default=0.99,
+        help="the discount factor gamma")
+    parser.add_argument("--batch-size", type=int, default=256,
+        help="the batch size of sample from the reply memory")
+    parser.add_argument("--epsilon", type=float, default=1.,
+        help="epsilon")
+    parser.add_argument("--epsilon-end", type=float, default=0.07,
+        help="epsilon end")
+    parser.add_argument("--epsilon-decay", type=float, default=0.9999,
+        help="epsilon decay")
+    parser.add_argument("--num-epochs", type=int, default=8,
+        help="the number of epochs to update the policy")
+    parser.add_argument("--policy-frequency", type=int, default=2,
+        help="the frequency of training policy (delayed)")
+    parser.add_argument("--target-update-freq", type=int, default=1, # Denis Yarats' implementation delays this by 2.
+        help="the frequency of updates for the target nerworks")
+ 
+    # Arguments for DyLam
+    parser.add_argument("--reward-scaling", type=float, default=1., help="reward scaling factor")
+    parser.add_argument("--episodes-rb", type=int, default=10, help="number of episodes to calculate rb")
+    parser.add_argument("--rew-tau", type=float, default=0.995, help="dylam tau")
+    parser.add_argument("--dylam", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True, help="Rather use DyLam or not")
+    parser.add_argument("--stratified", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True, help="Rather use stratified rewards or not")
+
+    args = parser.parse_args()
+    with open("dylam_hyperparameters.json", "r") as config_file:
+        configs = json.load(config_file)
+    configs = configs[args.gym_id]
+    for key, value in configs.items():
+        setattr(args, key, value)
+    return args
+
+
+def main(args):
+    _display = Display(visible=0, size=(1400, 900))
+    _display.start()
+    if not args.stratified:
+        method_name = "DQN"
+    else:
+        if args.dylam:
+            method_name = "DQN_DyLam"
+        else:
+            method_name = "DQN_drQ"
+    exp_name = f"{method_name}_{int(time.time())}_{args.gym_id}"
+    project = "DyLam"
+    if args.seed == 0:
+        args.seed = int(time.time())
+    args.method = method_name.split("_")[1] if not args.stratified else method_name
+    wandb.init(
+        project=project,
+        name=exp_name,
+        entity="goncamateus",
+        config=vars(args),
+        monitor_gym=True,
+        mode=None if args.track else "disabled",
+        save_code=True,
+    )
+    print(vars(args))
+    writer = SummaryWriter(f"runs/{exp_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s"
+        % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    # env setup
+    try:
+        env = gym.make(args.gym_id, render_mode="rgb_array")
+    except gym.error.NameNotFound:
+        env = mo_gym.make(args.gym_id, render_mode="rgb_array")u
+
+    agent = DQNAgent(
+        args,
+        env.observation_space,
+        env.action_space,
+    )
+    start_time = time.time()
+
+    # TRY NOT TO MODIFY: training loop
+    epi_reward = np.zeros(args.num_rewards)
+    obs = env.reset()
+    log = {}
+    for global_step in range(args.total_timesteps):
+        # ALGO LOGIC: put the logic for the algo here
+        if global_step < args.learning_starts:
+            action = env.action_space.sample()
+        else:
+            action = agent.get_action(obs)
+
+        # TRY NOT TO MODIFY: execute the action and collect the next obs
+        next_obs, rewards, done, truncated, info = env.step(action)
+        if not args.stratified:
+            rewards = rewards.sum()
+        epi_reward = epi_reward + rewards
+
+        # TRY NOT TO MODIFY: save data to reply buffer; handle `terminal_observation`
+        agent.replay_buffer.add(obs, action, rewards, next_obs, done)
+
+        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
+        obs = next_obs
+            
+        if done or truncated:
+            agent.rb_rewards.add(epi_reward)
+            epi_reward = np.zeros(args.num_rewards)
+            print(
+                f"global_step={global_step}, episodic_return={info['Original_reward']}"
+            )
+            log.update({f"ep_info/reward_total": info["Original_reward"]})
+            writer.add_scalar(
+                "charts/episodic_return", info["Original_reward"], global_step
+            )
+            strat_rewards = [x for x in info.keys() if x.startswith("reward_")]
+            for key in strat_rewards:
+                log.update({f"ep_info/{key.replace('reward_', '')}": info[key]})
+                writer.add_scalar(
+                    f"charts/{key.replace('reward_', '')}", info[key], global_step
+                )
+
+        # ALGO LOGIC: training.
+        if global_step > args.learning_starts:
+            # DyLam
+            if args.dylam:
+                lambdas = (
+                    torch.ones(args.num_rewards).to(agent.device) / args.num_rewards
+                )
+            else:
+                lambdas = torch.Tensor(args.lambdas).to(agent.device)
+            r_max = torch.Tensor(args.r_max).to(agent.device)
+            r_min = torch.Tensor(args.r_min).to(agent.device)
+            rew_tau = args.rew_tau
+            if agent.last_epi_rewards.can_do() and args.dylam:
+                rew_mean_t = torch.Tensor(agent.last_epi_rewards.mean()).to(
+                    agent.device
+                )
+                if agent.last_rew_mean is not None:
+                    rew_mean_t = (
+                        rew_mean_t + (agent.last_rew_mean - rew_mean_t) * rew_tau
+                    )
+                dQ = torch.clamp((r_max - rew_mean_t) / (r_max - r_min), 0, 1)
+                expdQ = torch.exp(dQ) - 1
+                lambdas = expdQ / (torch.sum(expdQ, 0) + 1e-4)
+                agent.last_rew_mean = rew_mean_t
+
+            update_policy = global_step % args.policy_frequency == 0
+            if update_policy:
+                policy_loss = agent.update(args.batch_size, lambdas)
+
+            if global_step % 100 == 0:
+                for i in range(len(lambdas)):
+                    log.update({"lambdas/component_" + str(i): lambdas[i].item()})
+                    writer.add_scalar(
+                        "lambdas/component_" + str(i), lambdas[i].item(), global_step
+                    )
+                if update_policy:
+                    log.update({"losses/policy_loss": policy_loss.item()})
+                    writer.add_scalar(
+                        "losses/policy_loss", policy_loss.item(), global_step
+                    )
+                log.update(
+                    {
+                        "charts/SPS": int(global_step / (time.time() - start_time)),
+                    }
+                )
+                writer.add_scalar(
+                    "charts/SPS",
+                    int(global_step / (time.time() - start_time)),
+                    global_step,
+                )
+
+        wandb.log(log, global_step)
+    agent.save(f"models/{exp_name}/")
+    envs.close()
+    writer.close()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
